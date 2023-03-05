3827169df66a489923be19f812c1d3dc2c268d92
OberstMax <oberst1@yandex.ru>
2023-03-05 22:38:57 +0800

    commit

---
+++ src/com/codewars/SumMultiplesOf3Or5.java
@@ -0,0 +1,27 @@
+package com.codewars;
+
+
+public class SumMultiplesOf3Or5 {
+    /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+Note: If the number is a multiple of both 3 and 5, only count it once.
+Courtesy of projecteuler.net (Problem 1)
+*/
+
+    public int solution(int number) {
+        int sum = 0;
+        if (number <= 1) return 0;
+        for (int i = number-1; i >= 0 ; i--) {
+            if ((i % 3 == 0) || (i % 5 == 0)) sum += i;
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        SumMultiplesOf3Or5 summ = new SumMultiplesOf3Or5();
+        System.out.println(summ.solution(10));
+        System.out.println(summ.solution(-10));
+        System.out.println(summ.solution(2));
+    }
+}
