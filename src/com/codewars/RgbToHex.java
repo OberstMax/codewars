e4ec7afd23ee2613fcaf52809f4216923ed898e7
OberstMax <Oberst1@yandex.ru>
2023-03-10 22:03:23 +0800

    commit

--- src/com/codewars/RgbToHex.java
+++ src/com/codewars/RgbToHex.java
@@ -23,7 +23,14 @@ public class RgbToHex {
 
         else return (""+a.toString()+d.toString()+c.toString());
     }
-
+//if(r < 0) r = 0;
+//        if(g < 0) g = 0;
+//        if(b < 0) b = 0;
+//        if(r > 255) r = 255;
+//        if(g > 255) g = 255;
+//        if(b > 255) b = 255;
+//
+//        return String.format("%02X%02X%02X", r, g, b);
     public static void main(String[] args) {
         System.out.println(RgbToHex.rgb(-20, 300, 11));
     }
